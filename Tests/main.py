9a7688d44f68f6a00acaa8d68eeb469a6e92139d
Jan Prusinowski <jprusinowski@gmail.com>
2024-03-29 09:21:23 +0100

    Fixed tests... Ugly but working.

--- Tests/main.py
+++ Tests/main.py
@@ -33,8 +33,7 @@ class SubnetManagerTester:
     def test_del(self):
         # Test deleting existing subnets
         self.report += "Testing deleting existing subnets:\n"
-        self.run_test("add 10.20.0.0/16", "Deleting existing subnet", "Adding 10.20.0.0/16", "0")
-        self.run_test("del 10.20.0.0/16", "Deleting existing subnet", "Deleting 10.20.0.0/16", "0")
+        self.run_test("add 10.20.0.0/16\ndel 10.20.0.0/16", "Deleting existing subnet", "Deleting 10.20.0.0/16", "0\n0")
         self.report += "\n"
 
         # Test deleting non-existing subnets
@@ -45,11 +44,7 @@ class SubnetManagerTester:
     def test_check(self):
         # Test checking IP addresses
         self.report += "Testing checking IP addresses:\n"
-        self.run_test("add 10.20.0.0/16", "Checking IP address", "Adding 10.20.0.0/16", "0")
-        self.run_test("add 32.64.128.0/20", "Checking IP address", "Adding 32.64.128.0/20", "0")
-        self.run_test("check 10.20.128.10", "Checking IP address", "Checking 10.20.128.10", "16")
-        self.run_test("check 32.64.140.200", "Checking IP address", "Checking 32.64.140.200", "20")
-        self.run_test("check 192.168.1.1", "Checking IP address", "Checking 192.168.1.1", "-1")
+        self.run_test("add 10.20.0.0/16\nadd 32.64.128.0/20\ncheck 10.20.128.10\ncheck 32.64.140.200\ncheck 192.168.1.1", "Checking IP address", "Adding 10.20.0.0/16", "0\n0\n16\n20\n-1")
         self.report += "\n"
 
     def run_all_tests(self):
